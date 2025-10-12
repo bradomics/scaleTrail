977b9962a30976948aef0bb1e55fe9d05a52fb8f
Brad Hall <bradomics@yahoo.com>
2025-10-11 23:49:12 -0500

    Added more setup prompts

--- cli/scaletrail/cli.py
+++ cli/scaletrail/cli.py
@@ -1,5 +1,7 @@
 import typer
 from pathlib import Path
+from enum import Enum
+
 
 from rich.console import Console
 from rich_pyfiglet import RichFiglet
@@ -25,7 +27,10 @@ def show_banner():
     console.print("* [link=https://github.com/bradomics/scaleTrail?tab=readme-ov-file#scaletrail]README[/link]")
     console.print("")
 
-    
+class NetworkType(str, Enum):
+    simple = "simple"
+    convolutional = "convolutional"
+    recurrent = "recurrent"
 
 @app.command()
 def init(
@@ -37,6 +42,30 @@ def init(
         "",
         help="The Linode region slug for your desired infrastructure's region.",
     ),
+    instance_type: str = typer.Option(
+        "",
+        help="The Linode instance type for your desired infrastructure.",
+    ),
+    image: str = typer.Option(
+        "",
+        help="The Linode image slug for your desired infrastructure's base image.",
+    ),
+    backups_enabled: bool = typer.Option(
+        False,
+        help="Whether to enable backups for the Linode instance.",
+    ),
+    tags: str = typer.Option(
+        "",
+        help="Comma-separated tags to apply to the Linode instance.",
+    ),
+    stripe_api_key: str = typer.Option(
+        "",
+        help="Your Stripe API key for payment processing.",
+    ),
+    sendgrid_api_key: str = typer.Option(
+        "",
+        help="Your SendGrid API key for payment processing.",
+    )
 ):
     """Initializes the project configuration."""
     # The banner and links are now part of the init command's execution flow.
@@ -59,6 +88,12 @@ def init(
 
     if not tags:
         tags = typer.prompt("Tags (comma-separated)")
+
+    if not stripe_api_key:
+        stripe_api_key = typer.prompt("Stripe API key")
+
+    if not sendgrid_api_key:
+        sendgrid_api_key = typer.prompt("SendGrid API key")
 
     config_dir = Path.home() / f".{project_name}"
     config_dir.mkdir(exist_ok=True)
